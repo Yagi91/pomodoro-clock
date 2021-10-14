895aecde455ca82481deb04abd04a406b93566f3
Yagi91 <bryantimah@gmail.com>
2021-10-14 15:33:25 +0100

    init

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
+    #POMODORO Clock or The 25+5 clock
+
+    *TABLE OF CONTENT*
+    - [Description of Project](#Description).
+    - [LINK to app]().
+    - [Tools Used and why I choose them.](#Tools).
+    - [User Stories and functionality of the clock](#USERS-STORY-COMPLETED-and-ADDED-FUNCTIONALITY-OF-THE-CLOCK).
+    - [Problems Encountered](#PROBLEMS-FACED).
+    - [Credit](#CREDITS).
+    - How to run the React.js App on your local machine.
+
+    ###Description
+    This app is countdown timer that can be used to work efficiently, with each session lasting 25 minutes by default and a break of 5 minutes by default, a the break and the working session alerts the user with a simple beep sound at the end.
+
+    ###TOOLs:
+    - React.js; as it fast to render and easily manage the various states in the app like when the clock is in a *counting* down or *paused*, a *session* or a *break*, and a combination of them.
+    - Sass; more efficient, enables you to use things like variables,
+    nested rules, inline imports and more
+    - HTML5/CSS6;
+
+    ## USERS STORY COMPLETED and ADDED FUNCTIONALITY OF THE CLOCK
+    - User is Able to set *break length* by incrementing or decrementing it.
+    - User is able to set *session length* by incrementing or decrementing it.
+    - User User can not set The *session* or *break* above 60 mins or below 1 mins
+    - User can start and stop the clock by clicking on the play/pause icon.
+    - The Time is displayed in `mm:ss`.
+    - User can continue the countdown from where there paused it after clicking the play icon.
+    - User can get a beep *audio* at the end of each session.
+    - User can set the app back to it's default state on clicking the reset button.
+    - *User can message the person who created the app using my twitter on my github profile description*
+
+    ###PROBLEMS FACED
+    In doing so I could say my greatest challenges was to create an accurate timer that is to make the count change after 1second be accurate since the `setInterval()` method can not be trusted.
+    Adding a ring that reduces it's length or circumference as times passes rationally depending on the minutes left. Tho this feature is still faulty after making the first count something I wish to fix when next I am updating the app.
+
+    ####CREDITS
+    This clock was created after completing my Front end libraries course on [FREE CODE CAMP](https://www.freecodecamp.org/).
+
+    ---
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
 ## Available Scripts
---
+++ src/components/clock.js
@@ -0,0 +1,275 @@
+import React from "react";
+import beep from "../media/beep.wav";
+import "../styles/calc.css";
+
+//challenging part was to create an accurate timer to execute at exact 1 second
+//I used a Dynamic timer timer that changes the time from 1second to about 0.92-0.96 millisecond dynamically based on the time it took to execute the function so as to better display the next time;
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mins: 25,
+      secs: 0,
+      breakLength: 5,
+      sessionLength: 25,
+      countdown: false,
+      session: "Session",
+      count: 25,
+    };
+    this.handleCount = this.handleCount.bind(this);
+    this.reset = this.reset.bind(this);
+    this.playRef = React.createRef();
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
+    this.manageStroke = this.manageStroke.bind(this);
+  }
+  handleCount() {
+    let timer = 1000; //timer initialized to 1000ms=1000s;
+    let endTime; //timestamp at the end of the code
+    let usedTime; //time used to run the code
+    this.setState((state) => ({
+      countdown: !state.countdown,
+      count: !state.count ? state.mins : state.count,
+    }));
+    let myInterval = setInterval(() => {
+      timer = 1000; //timer reset to give so as not to deviate and keep reducing the milliseconds but make 1000 the standard
+      let startTime = Date.now(); //records timestamp of code execution
+      if (this.state.countdown) {
+        if (this.state.secs > 0) {
+          this.setState((state) => ({ secs: state.secs - 1 }));
+        }
+        if (this.state.secs === 0) {
+          if (this.state.mins === 0) {
+            this.playRef.current.currentTime = 0;
+            this.playRef.current.play();
+            if (this.state.session === "Session") {
+              this.setState((state) => ({
+                session: "Break",
+                mins: state.breakLength,
+                secs: 0,
+                count: state.breakLength,
+              }));
+              console.log(this.state.count);
+            } else if (this.state.session === "Break") {
+              this.setState((state) => ({
+                session: "Session",
+                mins: state.sessionLength,
+                secs: 0,
+                count: state.sessionLength,
+              }));
+              console.log(this.state.count);
+            }
+          } else {
+            this.setState((state) => ({ mins: state.mins - 1, secs: 59 }));
+          }
+        }
+        endTime = Date.now(); //time stamp code completed execution
+        usedTime = endTime - startTime; //time ued for code to execute
+        timer -= usedTime; // time to use for the next operation which is a few milliseconds less to make it more accurate
+        //console.log(endTime, startTime, usedTime, startTime > endTime, timer);
+        //expected output: both start and endTime appear in milliseconds, endTime is larger than startTime , used time is a very mostly less than 10, false, timer<1000;
+      } else {
+        clearInterval(myInterval);
+      }
+    }, timer);
+  }
+  reset() {
+    this.playRef.current.currentTime = 0;
+    clearInterval(this.handleCount);
+    this.setState((state) => ({
+      countdown: false,
+      mins: 25,
+      secs: 0,
+      session: "Session",
+      breakLength: 5,
+      sessionLength: 25,
+      count: "25",
+    }));
+  }
+  decrement(e) {
+    if (!this.state.countdown) {
+      if (e.currentTarget.id === "break-decrement") {
+        if (this.state.breakLength > 1 && this.state.breakLength <= 60) {
+          this.setState((state) => ({
+            breakLength: state.breakLength - 1,
+            mins:
+              state.session === "Break" ? state.breakLength - 1 : state.mins,
+            secs: state.session === "Break" ? 0 : state.secs,
+            count:
+              state.session === "Break" ? state.breakLength - 1 : state.count,
+          }));
+        }
+      } else {
+        if (this.state.sessionLength > 1 && this.state.sessionLength <= 60) {
+          this.setState((state) => ({
+            sessionLength: state.sessionLength - 1,
+            mins:
+              state.session === "Session"
+                ? state.sessionLength - 1
+                : state.mins,
+            secs: state.session === "Session" ? 0 : state.secs,
+            count:
+              state.session === "Session"
+                ? state.sessionLength - 1
+                : state.count,
+          }));
+        }
+      }
+    }
+  }
+  increment(e) {
+    if (!this.state.countdown) {
+      console.log(e.currentTarget.id);
+      if (e.currentTarget.id === "break-increment") {
+        if (this.state.breakLength >= 1 && this.state.breakLength < 60) {
+          this.setState((state) => ({
+            breakLength: state.breakLength + 1,
+            mins:
+              state.session === "Break" ? state.breakLength + 1 : state.mins,
+            secs: state.session === "Break" ? 0 : state.secs,
+            count:
+              state.session === "Break" ? state.breakLength + 1 : state.count,
+          }));
+        }
+      } else {
+        if (this.state.sessionLength >= 1 && this.state.sessionLength < 60) {
+          this.setState((state) => ({
+            sessionLength: state.sessionLength + 1,
+            mins:
+              state.session === "Session"
+                ? state.sessionLength + 1
+                : state.mins,
+            secs: state.session === "Session" ? 0 : state.secs,
+            count:
+              state.session === "Session"
+                ? state.sessionLength + 1
+                : state.count,
+          }));
+        }
+      }
+    }
+  }
+  manageStroke() {
+    let offset = 794.83 - (this.state.mins / this.state.count) * 794.83;
+    return offset;
+  }
+  render() {
+    const style = {
+      circle: {
+        strokeDasharray: "794.83",
+        strokeDashoffset: this.manageStroke(),
+        transition: `all linear ${this.state.countdown ? "60s" : "0.3s"}`,
+      },
+    };
+    return (
+      <div className="clock">
+        <audio src={beep} id="beep" ref={this.playRef} />
+        <h3 className="header">POMODORO CLOCK</h3>
+        <Break
+          className="break"
+          decrement={this.decrement}
+          breakLength={this.state.breakLength}
+          increment={this.increment}
+        />
+        <Session
+          className="session"
+          decrement={this.decrement}
+          sessionLength={this.state.sessionLength}
+          increment={this.increment}
+        />
+        <section className="main">
+          <svg className="progress-ring" height="100%" width="100%">
+            <g>
+              <ellipse
+                style={style.circle}
+                className="progress-ring_circle"
+                ry="126"
+                rx="127"
+                cx="50%"
+                cy="53%"
+              />
+              {/* <path
+                id="base-timer-path-remaining"
+                stroke-dasharray="283"
+                class="base-timer__path-remaining ${remainingPathColor}"
+                d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+              ></path> */}
+            </g>
+          </svg>
+          <p id="timer-label">{this.state.session}</p>
+          <div id="time-left">
+            {this.state.mins < 10 ? `0${this.state.mins}` : this.state.mins}:
+            {this.state.secs < 10 ? `0${this.state.secs}` : this.state.secs}
+          </div>
+        </section>
+        <StartStop
+          countdown={this.state.countdown}
+          count={this.handleCount}
+          reset={this.reset}
+          className="startStop"
+        />
+      </div>
+    );
+  }
+}
+
+function Break(props) {
+  return (
+    <div className="break">
+      <div id="break-label">Break length</div>
+      <div className="break-buttons">
+        <button id="break-increment" onClick={props.increment}>
+          <i className="fa fa-arrow-up fa-2x" />
+        </button>
+        <p id="break-length">{props.breakLength}</p>
+        <button id="break-decrement" onClick={(e) => props.decrement(e)}>
+          <i className="fa fa-arrow-down fa-2x" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Session(props) {
+  return (
+    <div className="session">
+      <div id="session-label">Session length</div>
+      <div className="session-buttons">
+        <button id="session-increment" onClick={props.increment}>
+          <i className="fa fa-arrow-up fa-2x" />
+        </button>
+        <p id="session-length">{props.sessionLength}</p>
+        <button id="session-decrement" onClick={props.decrement}>
+          <i className="fa fa-arrow-down fa-2x" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function StartStop(props) {
+  return (
+    <div className="startStop">
+      {props.countdown ? (
+        <button id="start_stop" onClick={props.count}>
+          <i class="fa fa-pause fa-3x"></i>
+        </button>
+      ) : (
+        <button id="start_stop" onClick={props.count}>
+          <i class="fa fa-play fa-3x"></i>
+        </button>
+      )}
+      <button id="reset" onClick={props.reset}>
+        <i class="fa fa-refresh fa-3x" aria-hidden="true"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Clock;
